13c3beff79416a4bb7bd6e967210c95964891da5
WMS <1003743375@qq.com>
2021-09-25 21:10:44 +0800

    1

---
+++ Test/src/cn/ml/test/OneTest.java
@@ -0,0 +1,11 @@
+package cn.ml.test;
+
+/**
+ * @auther Delmer
+ * @create 2021-09-25 20:29
+ */
+public class OneTest {
+    public static void main(String[] args) {
+        System.out.println("This is first");
+    }
+}
